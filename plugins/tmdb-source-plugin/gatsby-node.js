3e7b229f6c73becb99e0c37a61b6ba2fa3376eb3
181221 <pederwaa@ifi.uio.no>
2020-01-14 00:46:56 +0100

    implement cache

--- plugins/tmdb-source-plugin/gatsby-node.js
+++ plugins/tmdb-source-plugin/gatsby-node.js
@@ -6,9 +6,11 @@ const fetch = require('node-fetch');
 const { createRemoteFileNode } = require('gatsby-source-filesystem');
 
 exports.sourceNodes = async (
-  { actions: { createNode }, createContentDigest, store, cache, createNodeId },
+  { actions: { createNode, touchNode }, createContentDigest, store, cache, createNodeId },
   configOptions,
 ) => {
+  // Create nodes here, generally by downloading data
+  // from a remote API.
   if (!configOptions.key) {
     throw new Error('no tmdb api key found');
   }
@@ -30,57 +32,77 @@ exports.sourceNodes = async (
     };
     return nodeData;
   };
-  const pageNr = configOptions.pageNr ? configOptions.pageNr : 2;
-  const promises = [];
+
   const asyncStuff = async url => {
     const res = await fetch(url);
     const json = await res.json();
     return json;
   };
-  for (let i = 0; i < pageNr; i++) {
-    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${configOptions.key}&page=${i +
-      1}`;
-    promises.push(url);
-  }
-  const data = await Promise.all(promises.map(el => asyncStuff(el))).then(responses => {
-    const tmdbPromis = responses.map(async response => {
-      const proms = [];
-      for (let i = 0; i < response.results.length; i++) {
-        const url = `https://api.themoviedb.org/3/movie/${response.results[i].id}/similar?api_key=${configOptions.key}`;
-        proms.push(url);
-      }
-      const similar = await Promise.all(proms.map(el => asyncStuff(el))).then(res =>
-        res.map(el => el.results),
-      );
-      response.results.map((el, index) => {
-        el.similar = similar[index];
-      });
-      return response;
-    });
-    return Promise.all(tmdbPromis);
-  });
-  const datatmdb = [];
-  data.map(el => {
-    datatmdb.push(...el.results);
-  });
 
+  let datatmdb = await cache.get('cache-data');
+  const pageNr = configOptions.pageNr ? configOptions.pageNr : 2;
+  const promises = [];
+  if (!datatmdb) {
+    console.log('no elements in cache');
+    console.log('doing calls');
+    datatmdb = [];
+    for (let i = 0; i < pageNr; i++) {
+      const url = `https://api.themoviedb.org/3/movie/popular?api_key=${
+        configOptions.key
+      }&page=${i + 1}`;
+      promises.push(url);
+    }
+    const data = await Promise.all(promises.map(el => asyncStuff(el))).then(responses => {
+      const tmdbPromis = responses.map(async response => {
+        const proms = [];
+        for (let i = 0; i < response.results.length; i++) {
+          const url = `https://api.themoviedb.org/3/movie/${response.results[i].id}/similar?api_key=${configOptions.key}`;
+          proms.push(url);
+        }
+        const similar = await Promise.all(proms.map(el => asyncStuff(el))).then(res =>
+          res.map(el => el.results),
+        );
+        response.results.map((el, index) => {
+          el.similar = similar[index];
+        });
+        return response;
+      });
+      return Promise.all(tmdbPromis);
+    });
+    data.map(el => {
+      datatmdb.push(...el.results);
+    });
+    await cache.set('cache-data', datatmdb);
+  }
   return new Promise(res => {
     datatmdb.map(async el => {
       const nodeData = processData(el);
-      try {
-        const fileNode = await createRemoteFileNode({
-          url: `http://image.tmdb.org/t/p/original${nodeData.poster_path}`, // string that points to the URL of the image
-          parentNodeId: nodeData.id, // id of the parent node of the fileNode you are going to create
-          createNode, // helper function in gatsby-node to generate the node
-          createNodeId, // helper function in gatsby-node to generate the node id
-          cache, // Gatsby's cache
-          store, // Gatsby's redux store
-        });
-        if (fileNode) {
-          nodeData.local_poster_path___NODE = fileNode.id;
+
+      let imageNodeId;
+      const remoteDataCacheKey = nodeData.id;
+      const cacheRemoteData = await cache.get(remoteDataCacheKey);
+      if (cacheRemoteData) {
+        imageNodeId = cacheRemoteData.imageNodeId; // eslint-disable-line prefer-destructuring
+        touchNode({ nodeId: cacheRemoteData.imageNodeId });
+      }
+      if (!imageNodeId) {
+        try {
+          const fileNode = await createRemoteFileNode({
+            url: `http://image.tmdb.org/t/p/original${nodeData.poster_path}`, // string that points to the URL of the image
+            parentNodeId: nodeData.id, // id of the parent node of the fileNode you are going to create
+            createNode, // helper function in gatsby-node to generate the node
+            createNodeId, // helper function in gatsby-node to generate the node id
+            cache, // Gatsby's cache
+            store, // Gatsby's redux store
+          });
+          if (fileNode) {
+            nodeData.local_poster_path___NODE = fileNode.id;
+
+            await cache.set(remoteDataCacheKey, nodeData);
+          }
+        } catch (error) {
+          console.log(error);
         }
-      } catch (error) {
-        console.log(error);
       }
       createNode(nodeData);
       res(nodeData);
