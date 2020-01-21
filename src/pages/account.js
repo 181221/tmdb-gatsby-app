9fd37753f7b1cd3201de5253a4c49bc856020666
181221 <pederwaa@ifi.uio.no>
2020-01-21 23:28:46 +0100

    clean up

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,29 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Router } from '@reach/router';
-import { login, isAuthenticated, getProfile } from '../utils/auth';
+import { login, isAuthenticated } from '../utils/auth';
 import Discover from '../components/movie/discover';
 import Movie from '../components/movie/movie';
 import PrivateRoute from '../components/privateRoute';
-import {
-  radarr_url,
-  account_movie,
-  landing,
-  account_request,
-  account_admin_request,
-} from '../constants/route';
+import { account_movie, landing } from '../constants/route';
 
 const Account = () => {
-  const [collection, setCollection] = useState(undefined);
-  const user = getProfile();
-  useEffect(() => {
-    const url_collection = `${radarr_url}/movie?apikey=${process.env.RADARR_API_KEY}`;
-    fetch(url_collection)
-      .then(res => res.json())
-      .then(json => {
-        setCollection(json);
-      })
-      .catch(err => console.error(err));
-  }, [user]);
   if (!isAuthenticated()) {
     login();
     return <p>Redirecting to login...</p>;
