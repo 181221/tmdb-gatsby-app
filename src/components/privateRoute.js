21579ed38c73da4f4be2e30c20b3d4f0bbeeb6c3
181221 <pederwaa@ifi.uio.no>
2020-01-22 16:21:05 +0100

    fix login

--- src/components/privateRoute.js
+++ src/components/privateRoute.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { navigate } from 'gatsby';
-import { isAuthenticated } from '../utils/auth';
+import { isAuthenticated, login } from '../utils/auth';
 
 const PrivateRoute = ({ component: Component, location, ...rest }) => {
-  if (!isAuthenticated() && location.pathname !== `/account/login`) {
-    navigate('/account/login');
+  if (!isAuthenticated()) {
+    login();
     return null;
   }
   // eslint-disable-next-line react/jsx-props-no-spreading
