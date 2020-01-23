3852ab61fc08e1074cb07cab4ee1addf9f5046fb
181221 <pederwaa@ifi.uio.no>
2020-01-23 14:28:37 +0100

    add template for service worker

--- src/custom-sw-code.js
+++ src/custom-sw-code.js
@@ -1,8 +1,41 @@
+/**
+ * This is a script appended to the service worker. You will have noe access 
+ * to dom elements in this script.
+ * Notification.requestPermission().then(per => {
+    console.log(per);
+  });
+ * should be done in the react app
+ * 
+ */
+
+/*
+
+if (!('Notification' in self.window)) {
+  console.log('shits going down');
+  alert('This browser does not support desktop notification');
+}
+
+console.log(Notification.permission !== 'denied');
 setTimeout(() => {
-  self.Notification.requestPermission().then(value => {
-    console.log('value', value);
+  // Another things
+  self.addEventListener('push', function(e) {
+    const user = localStorage.getItem('user');
+    console.log(user);
+    const options = {
+      body: `${user} This notification was generated from a push!`,
+      vibrate: [100, 50, 100],
+      actions: [
+        {
+          action: 'explore',
+          title: 'Explore this new world',
+        },
+        {
+          action: 'close',
+          title: 'Close',
+        },
+      ],
+    };
+    e.waitUntil(self.registration.showNotification('Hello world!', options));
   });
-  self.addEventListener('push', event => {
-    console.log(event);
-  });
-}, 3000);
+});
+*/
