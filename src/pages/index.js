2609facdceb3c4fabb8dbb55b1732cbf8fa57662
181221 <pederwaa@ifi.uio.no>
2020-03-21 13:51:30 +0100

    added a background picture

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 import Img from 'gatsby-image';
+import Typography from '@material-ui/core/Typography';
 import { graphql } from 'gatsby';
 
 const BackgroundContainer = styled.div``;
 
+const StyledImg = styled(Img)`
+  background-size: cover;
+  background-position: center;
+  height: 91vh;
+`;
 export default ({ data }) => {
   return (
     <BackgroundContainer>
-      {
-        // <Img fixed={data.file.childImageSharp.fixed} />
-      }
+      <StyledImg title="astronaut" fluid={data.file.childImageSharp.fluid} />
+      <Typography
+        variant="h1"
+        component="h1"
+        style={{ position: 'fixed', top: '50%', opacity: '0.8' }}
+      >
+        Requesterr
+      </Typography>
     </BackgroundContainer>
   );
 };
@@ -19,8 +30,8 @@ export const query = graphql`
   query {
     file(relativePath: { eq: "background.jpg" }) {
       childImageSharp {
-        fixed {
-          ...GatsbyImageSharpFixed
+        fluid {
+          ...GatsbyImageSharpFluid
         }
       }
     }
